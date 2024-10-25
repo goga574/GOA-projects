c6e67823f2e62fc9caff60a6b4d8672e8fc3c89e
goga574 <discordforgoa@gmail.com>
2024-10-25 17:09:48 +0400

    second commit

---
+++ level 07/homework/day7hw5.py
@@ -0,0 +1,11 @@
+#შექმენით ორი ცვლადი თენი სახელი და გვარი და მოახდინეთმ მათი კონკატენაცია
+
+name = "goga"
+
+surname = "chalauri"
+
+adress = "tbilisi"
+
+studying = "GOA"
+
+print(name + " " + surname + " " + adress + " " + studying)
