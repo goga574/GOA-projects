01ae8c1a26827befeabac298323a130412cf99a5
goga574 <discordforgoa@gmail.com>
2024-10-29 17:00:04 +0400

    fourth commit

---
+++ level 08/homework/day8hw0.py
@@ -0,0 +1,13 @@
+a = 5
+
+b = "10"
+
+result = a + int(b)
+
+print(result)
+
+
+
+
+
+#გვაქვს errot რისი მიზეზიც არის ის რომ variable-a არის integer და variable-b არის string,  str და int ერთანეთს არ ემატება
---
+++ level 08/homework/day8hw1.py
@@ -0,0 +1,7 @@
+value = "10" 
+
+result = int(value) * 5
+
+print(result)
+
+#value არის str ამიტომაც სტრინგის ინტეჯერზე გამრავლება არ შეგვიძლია ამიტომაც value გადავაქციოთ int ად
