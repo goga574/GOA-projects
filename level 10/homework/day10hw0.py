5df67b2be20b25ef1348f9e9cdb7ae130dd82fbb
goga574 <discordforgoa@gmail.com>
2024-11-18 15:12:22 +0400

    1

---
+++ level 10/homework/day10hw0.py
@@ -0,0 +1,9 @@
+#  დაწერეთ პროგრამა, რომელიც სთხოვს მომხმარებელს მის სახელს და შემდეგ მას მიესალმება. 
+
+name = input("what is your name?:")
+
+surname = input("what is your surname?")
+
+
+print("გამარჯობა" + " " + name + " " + surname)
+
---
+++ level 10/homework/day10hw2.py
@@ -0,0 +1,7 @@
+# დაწერე პროგრამა, რომელიც შემოატანინებს მომხმარებელს ორი რიცხვს და დაბეჭდავს მათ სხვაობას
+
+num1 = int(input("first number:"))
+
+num2 = int(input("second number:"))
+
+print(num1 - num2)
