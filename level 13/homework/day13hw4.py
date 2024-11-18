342c79667e5d37dbe84b1e2a7627c9d530cac0e1
goga574 <discordforgoa@gmail.com>
2024-11-18 21:35:06 +0400

    1

---
+++ level 13/homework/day13hw4.py
@@ -0,0 +1,9 @@
+# 5) მომხმარებელს შემოატაინეთ სახელი, გვარი,ასაკი, საცხოვრებელი ადგილი .
+
+name = input("what is your name?:")
+
+surname = input("what is your surname?:")
+
+age = int(input("what is your age?:"))
+
+living_place = input("where do you live?:")
