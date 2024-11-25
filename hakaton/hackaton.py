c770fee8572f79957f0daf3278ba8ba9c0cc6c06
goga574 <discordforgoa@gmail.com>
2024-11-25 16:51:10 +0400

    1

--- hakaton/hackaton.py
+++ hakaton/hackaton.py
@@ -1,3 +1,9 @@
 num1 = 10
 
-num2 = input("put numbers")
+
+num2 = input("put numbers:")
+
+if num2 == "10":
+    print("congrats,you guessed the number")
+elif num2 != "10":
+    print("try again")
